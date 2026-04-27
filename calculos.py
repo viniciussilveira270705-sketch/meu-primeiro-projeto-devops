bef33b614c70652e86e8467cc19dd573c0adda6a
viniciussilveira270705-sketch <viniciussilveira270705@gmail.com>
2026-04-27 20:43:17 -0300

    Create calculos.py

---
+++ calculos.py
@@ -0,0 +1,14 @@
+def soma(a, b):
+    return a + b
+
+def subtrai(a, b):
+    return a - b
+
+def multiplica(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def potencia(a, b):
+    return a ** b
